043b3d6594a7205deda1f66c4bd747de0326b5bb
gumci <dosms12@naver.com>
2016-07-28 18:14:06 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,56 @@
+package Mail;
+
+import java.util.Scanner;
+
+import javax.mail.MessagingException;
+
+public class main {
+	public static void main(String[] args) throws MessagingException {
+		// TODO Auto-generated method stub
+		String get;
+		String send;
+		String title;
+		String body;
+		
+		String st;
+		
+		boolean stop = true;
+			
+		while(stop == true){
+			Scanner scan = new Scanner(System.in);
+		
+			System.out.print("받는 사람 : ");		
+			get= scan.nextLine();
+			System.out.println("");
+			System.out.print("보내는 사람 : ");
+			send = scan.nextLine();
+			System.out.println("");
+			System.out.print("제목 입력 : ");
+			title = scan.nextLine();
+			System.out.println("");
+			System.out.print("내용 입력 : ");
+			body = scan.nextLine();
+			System.out.println("");
+			
+			mail mail = new mail(get, send, title, body); // 받는사람, 보내는사람, 제목, 내용
+			
+			System.out.println("##################################################################################################");
+			System.out.print("메일을 또 보내시겠습니까? 1. Yes 2. No : ");
+			st = scan.nextLine();
+			if(st.equals("1")== true || st.equals("yes")== true){
+			}
+			else{
+				stop = false;
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+// 아직은 시간이많이걸려요
---
+++ send.java
@@ -0,0 +1,42 @@
+package Mail;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class send {
+
+	void send(String na, String neo, String jaemok, String naeyoung){
+
+	     Properties props = new Properties();
+	     props.put("mail.transport.protocol", "smtp");
+	     props.put("mail.smtp.host", "223.130.121.106");
+	     props.put("mail.smtp.port", "25");
+	     
+	     Session session = Session.getDefaultInstance(props);
+	     
+	     try{
+	    	 MimeMessage message = new MimeMessage(session);
+	    	 InternetAddress to = new InternetAddress(neo);
+	    	 message.setFrom(to);
+	    	 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(na));
+	    	 message.setSubject(jaemok);
+	    	 message.setContent(naeyoung, "text/plain; charset=utf-8");
+	    	 message.setSentDate(new Date());
+
+	    	 Transport.send(message);
+	     }
+	     catch (MessagingException mex){
+	    	 mex.printStackTrace();	    	 
+	     }
+	     catch(Exception e){
+	    	 e.printStackTrace();
+	     }
+	}	
+}
+
