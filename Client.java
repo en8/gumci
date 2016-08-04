a016bf15051f8a12341eee5cc8ff1578a47fe343
gumci <dosms12@naver.com>
2016-08-04 18:04:52 +0900

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,54 @@
+package Client;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+			Socket sock = new Socket("192.168.59.45", 10000);
+			
+			OutputStream out = sock.getOutputStream();
+			
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+			String to = "";
+			String from = "";
+			String title = "";
+				
+			Scanner scan = new Scanner(System.in);
+			
+			while(to.matches("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$") == false || from.matches("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$") == false || title.matches("") == true ){
+				System.out.print("수신자 : ");
+				to = scan.nextLine();
+				System.out.println("");
+				System.out.print("발신자 : ");
+				from = scan.nextLine();
+				System.out.println("");
+				System.out.print("제목 입력 : ");
+				title = scan.nextLine();
+				System.out.println("");
+			}
+			
+			System.out.print("내용 입력 : ");
+			String body = scan.nextLine();
+			System.out.println("");
+			
+			pw.println(to);
+			pw.println(from);
+			pw.println(title);
+			pw.println(body);
+			
+			pw.flush();
+			sock.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		System.out.println(e.getMessage());
+		}
+	}
+}
